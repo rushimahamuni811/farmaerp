6dc5cf3fa3a35bacdd827a062aa9433cc41e4a4a
ifc <ifc@ifc-PC>
2023-01-19 18:53:29 +0530

    firstcommit

---
+++ farm/Inheritence/src/com/omvsab/Newinherts.java
@@ -0,0 +1,11 @@
+package com.omvsab;
+
+public interface Newinherts {
+	interface newkey{
+		void print();
+	
+	}
+
+
+	
+}		
---
+++ farm/Inheritence/src/com/omvsab/inhert.java
@@ -0,0 +1,12 @@
+package com.omvsab;
+
+public class inhert {
+	class inheritence extends inhert{
+
+	inheritence i= new inheritence();
+	
+		// TODO Auto-generated method stub
+
+	}
+
+}
